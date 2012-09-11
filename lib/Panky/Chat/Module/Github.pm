327a629002de86ee9fd1bdad12e8aeaf1efd56f1
William Wolf <throughnothing@gmail.com>
2012-09-11 01:03:09 -0700

    Updates to Github with Link Detection/shortening.

--- lib/Panky/Chat/Module/Github.pm
+++ lib/Panky/Chat/Module/Github.pm
@@ -5,20 +5,51 @@ use WWW::Shorten 'GitHub';
 
 # ABSTRACT: Handles github action requests from users in chatroom
 
+sub message {
+    my ($self, $msg, $from) = @_;
+
+    # Detect links to github
+    if( $msg =~ qr{(https?://github.com/(\S+))} ) {
+        my @parts = split qr{/}, $2, 5;
+
+        my $type;
+        given( \@parts ) {
+            # When its a commit
+            when ( @$_ > 2 and $_->[2] eq 'commit' ){
+                $type = substr( $parts[3], 0, 6)
+            }
+            # When its a pull request
+            when ( @$_ > 2 and $_->[2] eq 'pull' ) {
+                $type = "PR-$_->[3]";
+            }
+            # When its a blob or a tree
+            when ( @$_ > 2 and ( $_->[2] eq 'blob' || $_->[2] eq 'tree' ) ) {
+                $type = "($_->[3])/$_->[4]";
+            }
+        }
+
+        # Shorten found github links
+        my $link = makeashorterlink( $1 );
+
+        # Format message
+        $type = $type ? " $type" : '';
+        my $msg = "[$parts[0]/$parts[1]$type] $link";
+
+        $self->say( $msg );
+    }
+}
+
 sub directed_message {
     my ($self, $msg, $from) = @_;
     my $gh = $self->panky->github;
 
     given( $msg ) {
-        when( qr{(https?://github.com/\S+)} ) {
-            # Shorten found github links
-            my $link = makeashorterlink( $1 );
-            $self->say( $link );
-        }
+        # Setup a hook for a repo
         when( /gh setup (\S+)/ ) {
             $gh->create_hook( $1 );
             $self->say( "$from: Setup $1!" );
         }
+        # Test the specified hook
         when( /gh test-hooks (\S+)/ ) {
             $gh->test_hook( $1 );
         }
@@ -26,7 +57,3 @@ sub directed_message {
 }
 
 1;
-
-=head1 SYNOPSIS
-
-This is a dummy module that simply echoes what anyone says in the chatroom.
